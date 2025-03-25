40033b4f71ff72b382aa3cd4417d21e87beb50b6
Fabienholert <fabienholert@gmail.com>
2025-03-25 10:11:22 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div id="accueil" className="App">
       <ConfigModal />
-      <BrowserRouter basename="/fabien-portfolio">
+      <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/fabien-portfolio",
+  base: "/portfolio/",
 });
