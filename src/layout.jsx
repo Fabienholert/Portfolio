db7568f659346112f2b3ebd73a8b1932ddabc7db
Fabienholert <fabienholert@gmail.com>
2025-03-25 15:35:43 +0100

    Nettoyage du code : suppression des lignes vides et ajustement de l'espacement dans les fichiers layout.jsx et main.jsx

--- src/layout.jsx
+++ src/layout.jsx
@@ -3,16 +3,16 @@ import Modal from "react-modal";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-
 export default function Layout() {
   useEffect(() => {
-    Modal.setAppElement("#accueil"); // Configure Modal here, using the ID of the div in App.jsx
+    Modal.setAppElement("#accueil");
   }, []);
   return (
     <>
       <Header />
       <main>
-        <Outlet /> {/* Les routes enfants seront affichées ici */}
+        <Outlet />
+        {}
       </main>
       <Footer />
     </>
